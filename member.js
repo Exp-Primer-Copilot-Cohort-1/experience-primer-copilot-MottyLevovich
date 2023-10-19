50daede23a2349dfeed3aa6749693eadea216b72
Ariel Levovich <motty13@gmail.com>
2023-10-19 21:30:27 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,15 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var projects = document.getElementById("projects");
+    var contact = document.getElementById("contact");
+
+    if (member.style.display === "none") {
+        member.style.display = "block";
+        skills.style.display = "none";
+        projects.style.display = "none";
+        contact.style.display = "none";
+    } else {
+        member.style.display = "none";
+    }
+}
